307500aebbd9e7ec55180182094329fda01b921e
Justin Fortin <733915+fortinj1354@users.noreply.github.com>
2024-03-06 11:26:40 -0500

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -41,7 +41,7 @@
           <span class="text-muted">
             Last Updated: {{moment(updated).format('h:mm:ssa YYYY-MMM-DD')}} <br />
             Data powered by The Blue Alliance API <br />
-            Dashboard written by TechplexEngineer on GitHub with FRC Team 4909 with modifications by FRC Team 2974
+            Original dashboard written by FRC Team 4909 - TechplexEngineer/pipitdisplay on GitHub
           </span>
         </div>
   
