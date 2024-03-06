de3b6090bdec704bdeb73345dcc7cc46dec3c5aa
Justin Fortin <733915+fortinj1354@users.noreply.github.com>
2024-03-05 19:27:25 -0500

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container-fluid px-5 py-4">
-  <h1 class="text-center">TEST TEXT</h1>
+  <h1 class="text-center" style="font-size: 5rem">Walton Robotics | 2974</h1>
     <div class="row">
 
       <div class="col-lg-12 col-xl-8">
