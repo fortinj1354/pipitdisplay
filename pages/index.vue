84c2cd925eef8f9e629b9f16dc30ea54c7fda874
Justin Fortin <733915+fortinj1354@users.noreply.github.com>
2024-03-21 16:27:34 -0400

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -151,6 +151,8 @@
       <div style="flex: 1" class="sponsors">
         <img src="~/assets/novelis.png" />
         <img src="~/assets/carrier.png" />
+        <img src="~/assets/difco.webp" />
+        <img src="~/assets/ccsd.webp" />
       </div>
     </div>
   </div>
