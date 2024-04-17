ee33936cb80fabf5ce997aa8ca60eb9821cec8f5
Justin Fortin <733915+fortinj1354@users.noreply.github.com>
2024-04-17 08:37:24 -0400

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -162,7 +162,7 @@
         <img src="~/assets/carrier.svg" />
         <img src="~/assets/automated-logic.svg" />
         <img src="~/assets/difco.webp" />
-        <img src="~/assets/ccsd.webp" />
+        <img src="~/assets/ccsd-removebg.png" />
       </div>
     </div>
   </div>
